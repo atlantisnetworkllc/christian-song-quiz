34343623793bdefab6ad1814f5d986c81e8a98d3
atlantisnetworkllc <atlantis.network.llc@gmail.com>
2025-12-31 15:29:54 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'PrayerSong - Create Your Personalized Song',
+  description: 'Create a beautiful, personalized song for your loved ones',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
